e07f1be0e792a15a1de04af378259352d23f9ac2
Worh682 <danny990232@gmail.com>
2025-02-13 20:02:43 +0800

    v1.0

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
+
+// 按下按鈕之後產生的變化
+menu.onclick = () => {
+	menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+// 回到上層按鈕
+window.onscroll = () => {
+    if (window.scrollY > 60) {
+        document.querySelector('#scroll-up').classList.add('active');
+    } else {
+        document.querySelector('#scroll-up').classList.remove('active');
+    }
+}
+
+const scriptURL='https://script.google.com/macros/s/AKfycbxZ--5UbkqO-lP794rQ-zk1puoFgqW43N8iheuYCz0j-rFLXXYv8CnF7JuxVzxQau9R/exec';
+const form = document.forms['orderlist'];
+	  
+form.addEventListener('submit', e => {
+	e.preventDefault()
+	fetch(scriptURL,{method:'post',body: new FormData(form)})
+	.then(response=>alert("you have successfully submitted."))
+	.catch(error =>console.error('Error!', error.message))
+	
+	
+	
+	
+})
